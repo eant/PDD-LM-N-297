f4387d9327c5979f3f0d3b1d1641cc6366f7908d
smessina <silvio.messina@eant.tech>
2021-09-01 22:02:20 -0300

    Creando ruta estatica para acceso a contenido en formato JSON

--- Clase 11/demo.py
+++ Clase 11/demo.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json
 
 app = Flask(__name__)
 
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return "Hola mundo desde la home de <b>Flask</b> :D"
 
+
+@app.route("/users")
+def usersTwitter():
+    users = [
+        { "name" : "smessina_" },
+        { "name" : "eanttech" },
+        { "name" : "TinchoLutter" },
+        { "name" : "bitcoinarg" }
+    ]
+    
+    response = app.response_class(response = json.dumps(users), status = 200, mimetype = "application/json")
+
+    return response
 
 app.run( port = 3000, host = "0.0.0.0" )
