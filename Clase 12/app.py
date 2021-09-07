70b698be0ec840fcbfc3b28df96be7dd697fa8ab
smessina <info@silviomessina.com>
2021-09-06 21:28:51 -0300

    Conectando app a MongoDB Atlas

--- Clase 12/app.py
+++ Clase 12/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, json
-
+from pymongo import MongoClient
 app = Flask(__name__)
 
+### MongoDB ###
+client = MongoClient("mongodb+srv://eant-python:eantpass2021@python-data-developers.osipp.mongodb.net/PDD-MJ-N-287?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+###############
+
+###############
 @app.route("/")
 def home():
     return "Hola mundo desde la home de <b>Flask</b> :D"
+###############
 
-
+###############
 @app.route("/users")
 def usersTwitter():
     users = [
@@ -19,5 +25,36 @@ def usersTwitter():
     response = app.response_class(response = json.dumps(users), status = 200, mimetype = "application/json")
 
     return response
+###############
+
+###############
+@app.route("/tweets")
+def getTweets():
+    twitter = client['PDD-MJ-N-287']['twitter']
+    
+    users = twitter.find().limit(4)
+    
+    result = []
+    
+    for user in users:
+        item = {
+            'usuario' : user['name']
+        }
+        result.append(item)
+    
+    return app.response_class(response = json.dumps(result), status  = 200, mimetype = "application/json" )
+
+
+
+
+
+
+
+
+
+
+
+
+###############
 
 app.run( port = 3000, host = "0.0.0.0" )
